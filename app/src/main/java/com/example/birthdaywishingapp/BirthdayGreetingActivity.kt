13e7cf763d129482a6aee1083d6c8c32a568e48a
Sapna <sapnagulati9990@gmail.com>
2020-11-11 14:18:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthdaywishingapp/BirthdayGreetingActivity.kt
@@ -0,0 +1,20 @@
+package com.example.birthdaywishingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object {                             // static variables
+        const val NAME_EXTRA = "name_extra"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        birthdayGreetings.text = "Happy Birthday\n$name!"
+    }
+}
---
+++ app/src/main/java/com/example/birthdaywishingapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.birthdaywishingapp
+
+import android.bluetooth.BluetoothGatt
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        createBirthdayButton.setOnClickListener{
+//
+//        }
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name = nameInput.editableText.toString()
+//        Toast.makeText(this, "name is $name", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+    }
+}
